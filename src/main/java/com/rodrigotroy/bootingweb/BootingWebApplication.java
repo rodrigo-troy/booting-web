72f8bad9438dba3ad72acdb7dfb0833ede2bbc52
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-05-19 17:21:59 +0100

    Base code

--- src/main/java/com/rodrigotroy/bootingweb/BootingWebApplication.java
+++ src/main/java/com/rodrigotroy/bootingweb/BootingWebApplication.java
@@ -10,7 +10,7 @@ public class BootingWebApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(BootingWebApplication.class,
                                                                                               args);
-        System.out.printf("%s initialized",
+        System.out.printf("%s initialized\n",
                           configurableApplicationContext.getApplicationName());
     }
 
