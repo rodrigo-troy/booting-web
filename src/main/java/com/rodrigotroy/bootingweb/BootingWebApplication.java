09a6c4240b692269f7c570e1e0a8f197baa67a98
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-05-19 17:13:49 +0100

    Initial commit

--- src/main/java/com/rodrigotroy/bootingweb/BootingWebApplication.java
+++ src/main/java/com/rodrigotroy/bootingweb/BootingWebApplication.java
@@ -2,12 +2,16 @@ package com.rodrigotroy.bootingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BootingWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootingWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(BootingWebApplication.class,
+                                                                                                   args)) {
+            System.out.println("initializing...");
+        }
+    }
 
 }
